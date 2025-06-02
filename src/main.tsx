54ca487b709353cc7a5f0666cf88b764304731e2
shxhidtutorly <githubshxhid@gmail.com>
2025-06-02 22:36:10 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Create QueryClient
@@ -15,5 +15,9 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
