a30ca0e64754ec8cbd38b671a6943118fbe5e4ce
shxhidtutorly <githubshxhid@gmail.com>
2025-06-19 18:50:13 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import App from "./App.tsx";
 import "./index.css";
 
+// You MUST wrap with ClerkProvider at the top, and BrowserRouter only once!
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 );
