6d19404949f5604515c01cc3cfe6fcbfa9c5aeb1
shxhidtutorly <githubshxhid@gmail.com>
2025-06-21 00:22:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ClerkProvider, RedirectToSignIn } from "@clerk/clerk-react";
 import { useUser } from "@clerk/clerk-react";
@@ -64,7 +64,6 @@ const App = () => (
       <ThemeProvider>
         <TooltipProvider>
           
-            <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/signin" element={<SignInPage />} />
               <Route path="/signup" element={<SignUpPage />} />
@@ -100,7 +99,6 @@ const App = () => (
               <Route path="/privacy" element={<Privacy />} />
 
               <Route path="*" element={<NotFound />} />
-            </Routes>
       
           <Toaster />
           <Sonner />
