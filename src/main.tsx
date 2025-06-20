c855d8557b1814b9ba3a99eee5a6e69851b91984
shxhidtutorly <githubshxhid@gmail.com>
2025-06-21 00:25:27 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -16,9 +16,9 @@ if (!clerkPublishableKey) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={clerkPublishableKey}>
-      
+      <BrowserRouter>
         <App />
-      
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
