bc518f55abc6d87f08ba792a2522a2d4a32ff6bf
shxhidtutorly <githubshxhid@gmail.com>
2025-06-20 17:11:04 +0530

    Update useRealTimeData.tsx

--- src/hooks/useRealTimeData.tsx
+++ src/hooks/useRealTimeData.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import { useAuth } from '@/contexts/SupabaseAuthContext';
+import { useUser, useClerk } from "@clerk/clerk-react";
 import { supabase } from '@/integrations/supabase/client';
 import { useToast } from '@/components/ui/use-toast';
 import { getUserStudyMaterials } from '@/lib/database';
