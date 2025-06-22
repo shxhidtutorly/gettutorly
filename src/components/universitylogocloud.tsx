9c8d2cf24c1debecd8fa95bbb37ebf535faf50a6
shxhidtutorly <githubshxhid@gmail.com>
2025-06-23 01:13:20 +0530

    Update universitylogocloud.tsx

--- src/components/universitylogocloud.tsx
+++ src/components/universitylogocloud.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { Cloud, ICloud } from "react-icon-cloud"
+import { Cloud } from "react-icon-cloud"
 
 const universityLogos = [
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Harvard_University_logo.svg/200px-Harvard_University_logo.svg.png",
-    alt: "Harvard University",
+    alt: "Harvard",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/MIT_logo.svg/200px-MIT_logo.svg.png",
@@ -13,23 +13,23 @@ const universityLogos = [
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Stanford_University_seal_2003.svg/200px-Stanford_University_seal_2003.svg.png",
-    alt: "Stanford University",
+    alt: "Stanford",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Yale_University_Shield_1.svg/200px-Yale_University_Shield_1.svg.png",
-    alt: "Yale University",
+    alt: "Yale",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Princeton_shield.svg/200px-Princeton_shield.svg.png",
-    alt: "Princeton University",
+    alt: "Princeton",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Columbia_University_shield.svg/200px-Columbia_University_shield.svg.png",
-    alt: "Columbia University",
+    alt: "Columbia",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/University_of_Pennsylvania_coat_of_arms.svg/200px-University_of_Pennsylvania_coat_of_arms.svg.png",
-    alt: "University of Pennsylvania",
+    alt: "UPenn",
   },
   {
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Caltech_logo.svg/200px-Caltech_logo.svg.png",
@@ -39,34 +39,31 @@ const universityLogos = [
 
 export function UniversityLogoCloud() {
   return (
-    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8">
-      {/* @ts-ignore */}
-      <Cloud
-        options={{
-          depth: 1,
-          imageScale: 2,
-          tooltip: "native",
-          clickToFront: 500,
-          reverse: true,
-          outlineColour: "#0000",
-          maxSpeed: 0.05,
-          minSpeed: 0.02,
-        }}
-      >
-        {universityLogos.map((logo, i) => (
-          <a key={i} href="#" onClick={(e) => e.preventDefault()}>
-            <img
-              src={logo.src}
-              alt={logo.alt}
-              title={logo.alt}
-              width={42}
-              height={42}
-              className="rounded-sm object-contain"
-              style={{ borderRadius: "8px" }}
-            />
-          </a>
-        ))}
-      </Cloud>
-    </div>
+    // @ts-ignore
+    <Cloud
+      options={{
+        depth: 1,
+        imageScale: 2,
+        tooltip: "native",
+        clickToFront: 500,
+        reverse: true,
+        outlineColour: "#0000",
+        maxSpeed: 0.05,
+        minSpeed: 0.02,
+      }}
+    >
+      {universityLogos.map((logo, i) => (
+        <a key={i} href="#">
+          <img
+            src={logo.src}
+            alt={logo.alt}
+            title={logo.alt}
+            width={42}
+            height={42}
+            className="object-contain"
+          />
+        </a>
+      ))}
+    </Cloud>
   )
 }
