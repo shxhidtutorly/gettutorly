db36229308aad24ef002078e01fce838b4504fe5
shxhidtutorly <githubshxhid@gmail.com>
2025-06-23 01:45:38 +0530

    Update AudioNotes.tsx

--- src/pages/AudioNotes.tsx
+++ src/pages/AudioNotes.tsx
@@ -1,4 +1,4 @@
-
+import { supabase } from "@/integrations/supabase/client";
 import { useState, useEffect } from "react";
 import { useUser, useClerk } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
