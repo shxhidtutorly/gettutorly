384febb8a5557785b2b6329edc8d415d1c412a3d
shxhidtutorly <githubshxhid@gmail.com>
2025-07-01 16:20:39 +0530

    Update useSupabaseStorage.tsx

--- src/hooks/useSupabaseStorage.tsx
+++ src/hooks/useSupabaseStorage.tsx
@@ -1,6 +1,5 @@
 
 import { useState } from 'react';
-import { useUser, useClerk } from "@clerk/clerk-react";
 import { supabase } from '@/integrations/supabase/client';
 
 export const useSupabaseStorage = () => {
