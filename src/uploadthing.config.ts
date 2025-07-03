d818e5dcfd0aed2ff8acc9e652af7ddffdc69206
shxhidtutorly <githubshxhid@gmail.com>
2025-07-03 18:36:07 +0530

    Update uploadthing.config.ts

--- src/uploadthing.config.ts
+++ src/uploadthing.config.ts
@@ -1,13 +1,13 @@
-// uploadthing.config.ts
-
 import { createUploadthing, type FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  audioUploader: f({ audio: { maxFileSize: "20MB" } }).onUploadComplete(({ file }) => {
-    console.log("File uploaded:", file.url);
-  }),
+  audioUploader: f({ audio: { maxFileSize: "10MB" } })
+    .onUploadComplete(async ({ file }) => {
+      console.log("Uploaded file:", file.url);
+      return { uploadedBy: "tutorly-system" };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
