4d08ef17ef486a22da25386312701ce589ff0f39
shxhidtutorly <githubshxhid@gmail.com>
2025-07-08 01:06:37 +0530

    Delete src/utils/uploadthing.ts

--- src/utils/uploadthing.ts
+++
@@ -1,4 +0,0 @@
-import { generateComponents } from "@uploadthing/react";
-import type { OurFileRouter } from "@/uploadthing.config";
-
-export const { UploadButton, UploadDropzone } = generateComponents<OurFileRouter>();
