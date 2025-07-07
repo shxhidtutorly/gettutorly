a8c71b3b5bb9d45ea7a93a37d848784e8afbe388
shxhidtutorly <githubshxhid@gmail.com>
2025-07-08 01:06:17 +0530

    Delete src/pages/uploadthing.ts

--- src/pages/uploadthing.ts
+++
@@ -1,6 +0,0 @@
-import { createNextPageApiHandler } from "uploadthing/next";
-import { ourFileRouter } from "@/uploadthing.config";
-
-export default createNextPageApiHandler({
-  router: ourFileRouter,
-});
