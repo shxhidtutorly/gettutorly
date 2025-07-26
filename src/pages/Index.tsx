58496f98112939fabe08489d2a9f015ffd438539
shxhidtutorly <githubshxhid@gmail.com>
2025-07-26 19:00:47 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,8 +17,8 @@ import {
   CreditCard,
 } from "lucide-react"
 import { useState, useEffect } from "react"
-import { Navbar } from "@/components/Navbar"
-import { Footer } from "@/components/Footer"
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
 
 export default function LandingPage() {
   const [stickyNav, setStickyNav] = useState(false)
