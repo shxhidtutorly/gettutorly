34bcb7c60223b0dee899eecb2a4ddc32d5882dad
shxhidtutorly <githubshxhid@gmail.com>
2025-08-17 03:09:38 +0530

    Update i18n.ts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -7,13 +7,29 @@ import hi from './locales/hi.json';
 import kn from './locales/kn.json';
 import es from './locales/es.json';
 import fr from './locales/fr.json';
+import ar from './locales/ar.json';
+import de from './locales/de.json';
+import it from './locales/it.json';
+import ja from './locales/ja.json';
+import pt from './locales/pt.json';
+import ru from './locales/ru.json';
+import tl from './locales/tl.json';
+import tr from './locales/tr.json';
 
 const resources = {
   en: { translation: en },
   hi: { translation: hi },
   kn: { translation: kn },
   es: { translation: es },
-  fr: { translation: fr }
+  fr: { translation: fr },
+  ar: { translation: ar },
+  de: { translation: de },
+  it: { translation: it },
+  ja: { translation: ja },
+  pt: { translation: pt },
+  ru: { translation: ru },
+  tl: { translation: tl },
+  tr: { translation: tr }
 };
 
 i18n
